17abec686a6956c38d0a7356c55d063e3d971f8e
Agrim Singh <59444327+th-efool@users.noreply.github.com>
2025-06-17 08:14:03 +0530

    Initial commit

---
+++ Source/MyProject/ACGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "ACGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AACGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};

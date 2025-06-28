c1cefbe2bcb5cc3489cc6510c816d05dba94f90d
Hakaluka <prsaryanjain@gmail.com>
2025-06-29 02:34:06 +0530

    Added Movement Mechanics(without vertical flip fix)

--- Source/MyProject/AircraftMovementComponent.h
+++ Source/MyProject/AircraftMovementComponent.h
@@ -6,12 +6,37 @@
 #include "GameFramework/PawnMovementComponent.h"
 #include "AircraftMovementComponent.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class MYPROJECT_API UAircraftMovementComponent : public UPawnMovementComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UAircraftMovementComponent();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+
+	UPROPERTY(Transient)
+	FVector CurrentLinearVelocity;
+
+	UPROPERTY(Transient)
+	FRotator CurrentAngularVelocity;
+
+	FRotator DeltaRotation;
+
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Aircraft Movement")
+	void SetLinearVelocity(FVector NewVelocity);
+
+	UFUNCTION(BlueprintCallable, Category = "Aircraft Movement")
+	void SetAngularVelocity(FRotator NewAngularVelocity);
+
 };
